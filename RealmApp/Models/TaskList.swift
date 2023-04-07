067788c592f76ed41e3559ebbd24df92eda342c7
romkalan <romkalan@mail.ru>
2023-04-07 09:45:20 +0300

    add final in the classes

--- RealmApp/Models/TaskList.swift
+++ RealmApp/Models/TaskList.swift
@@ -9,13 +9,13 @@
 import RealmSwift
 import Foundation
 
-class TaskList: Object {
+final class TaskList: Object {
     @Persisted var title = ""
     @Persisted var date = Date()
     @Persisted var tasks = List<Task>()
 }
 
-class Task: Object {
+final class Task: Object {
     @Persisted var title = ""
     @Persisted var note = ""
     @Persisted var date = Date()
